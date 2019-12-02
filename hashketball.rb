d5453ab699a62502960900f20fd27c6a707c2ec9
melia miller <meliajm@gmail.com>
2019-12-02 21:05:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -321,8 +321,9 @@ def player_with_longest_name
   i = 0
   
     while i < game_hash[keys][:players].length do 
+      binding.pry
       if game_hash[keys][:players][i][:points][:player_name].length > longest_name
-        binding.pry
+        
         longest_name = game_hash[keys][:players][i][:points][:player_name].length
         name = game_hash[keys][:players][i][:points][:player_name]
       end
