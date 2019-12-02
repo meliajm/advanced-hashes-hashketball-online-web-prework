8c881b5cb082b20138be7a406273f00c1f95f8af
melia miller <meliajm@gmail.com>
2019-12-02 18:32:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -88,37 +88,37 @@ def game_hash
           :slam_dunks => 10
         },
         {
-          :player_name => "Brook Lopez",
-          :number => 11,
-          :shoe_size => 17,
-          :points => 17,
-          :rebounds => 19,
-          :assists => 10,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 15
+          :player_name => "DeSagna Diop",
+          :number => 2,
+          :shoe_size => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
         },
         {
-          :player_name => "Mason Plumlee",
-          :number => 1,
-          :shoe_size => 19,
-          :points => 26,
-          :rebounds => 11,
-          :assists => 6,
-          :steals => 3,
-          :blocks => 8,
-          :slam_dunks => 5
+          :player_name => "Ben Gordon",
+          :number => 8,
+          :shoe_size => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
         },
         {
           :player_name => "Jason Terry",
           :number => 31,
           :shoe_size => 15,
           :points => 19,
-          :rebounds => 2,
-          :assists => 2,
-          :steals => 4,
-          :blocks => 11,
-          :slam_dunks => 1
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 12
         }
       ]
     }
