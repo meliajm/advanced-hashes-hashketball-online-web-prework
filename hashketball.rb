48cbc870c4fa46ebf0731fc77ef70b0a060168d9
melia miller <meliajm@gmail.com>
2019-12-02 18:33:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -110,7 +110,7 @@ def game_hash
           :slam_dunks => 0
         },
         {
-          :player_name => "Jason Terry",
+          :player_name => "Kemba Walker",
           :number => 33,
           :shoe_size => 15,
           :points => 6,
