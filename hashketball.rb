3c4441a8dc0a42af1cc8a44f8c3c4c130092a0ce
melia miller <meliajm@gmail.com>
2019-12-02 19:17:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -142,7 +142,7 @@ end
 
 def num_points_scored(name)
   game_hash.each { |keys, values|
-    keys.each { |k, v|
+    values.each { |k, v|
       if game_hash[keys][:players][k][:player_name] == name
         game_hash[keys][:players][k][:points]
       end
