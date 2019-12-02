8afbccf05c6bfad0f1ab45cf53d349612333658b
melia miller <meliajm@gmail.com>
2019-12-02 18:12:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,29 +4,31 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => {
-        "Alan Anderson" => {},
-        "Reggie Evans" => {},
-        "Brook Lopez" => {},
-        "Mason Plumlee" => {},
-        "Jason Terry" => {}
+        
         }
       },
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => {
-        "Jeff Adrien" => {},
-        "Bismack Biyombo" => {},
-        "DeSagna Diop" => {},
-        "Ben Gordon" => {},
-        "Kemba Walker" => {}
+        
       }
     }
   }
 end
 
 
+# "Alan Anderson" => {},
+#         "Reggie Evans" => {},
+#         "Brook Lopez" => {},
+#         "Mason Plumlee" => {},
+#         "Jason Terry" => {}
 
+# "Jeff Adrien" => {},
+#         "Bismack Biyombo" => {},
+#         "DeSagna Diop" => {},
+#         "Ben Gordon" => {},
+#         "Kemba Walker" => {}
 
 
 
